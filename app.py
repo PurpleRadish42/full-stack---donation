cc2053a85e3c6376b680e30a7b18df70cc305a22
R Abhijit Srivathsan <abhijit.7472@gmail.com>
2024-11-30 19:27:46 +0530

    added a basic flask code and setup a SQL database

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 from flask import *
+from flask_mysqldb import MySQL
 
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'   
+app.config['MYSQL_PASSWORD'] = '@Bhijit6151'
+app.config['MYSQL_DB'] = 'donation'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
