d1fba2838a4ea893585dd2a4f05ab9c27ee67eb4
Abhijit <abhijit.7472@gmail.com>
2024-12-05 16:52:42 +0530

    python and homepage

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import *
 from flask_mysqldb import MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'   
-app.config['MYSQL_PASSWORD'] = '@Bhijit6151'
-app.config['MYSQL_DB'] = 'donation'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'   
+# app.config['MYSQL_PASSWORD'] = '@Bhijit6151'
+# app.config['MYSQL_DB'] = 'donation'
 
-mysql = MySQL(app)
+# mysql = MySQL(app)\
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+app.run(debug=True)
+
