e7a122cd4f34423fb2bf6c1b05a092b026d720fc
Emiya02 <emiyasusanjuby2002@gmail.com>
2024-12-12 19:46:14 +0530

    Update emiya_db.sql

--- emiya_db.sql
+++ emiya_db.sql
@@ -22,3 +22,22 @@ CREATE TABLE login (
 -- Insert an admin user into the login table
 INSERT INTO login (username, password, role) VALUES ('admin', 'admin123', 'admin');
 
+INSERT INTO login(username,password, role) VALUES ('emiya', 'emiya123', 'user');
+ALTER TABLE login
+MODIFY password
+varchar(255);
+
+select*from login;
+
+CREATE TABLE payments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    payment_method ENUM('Credit Card', 'Debit Card', 'UPI') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+select*from payments;
+
+
