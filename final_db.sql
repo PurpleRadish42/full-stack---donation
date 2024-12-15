77bec32fca07106b7f6859b4a9b045f0dfa98828
Abhijit <abhijit.7472@gmail.com>
2024-12-16 01:21:07 +0530

    Update final_db.sql

--- final_db.sql
+++ final_db.sql
@@ -78,3 +78,13 @@ create table centers(
     img2 text,
     img3 text,
     address text);
+
+alter table bookings add column email varchar(255);
+
+CREATE TABLE volunteers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    message TEXT NOT NULL,
+    submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
